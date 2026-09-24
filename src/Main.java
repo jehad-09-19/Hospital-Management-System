2c00a9d3bb1b22ebac39d18cc274c06a61e1a34e
Lamia Nusrat Mim <mimlamianusrat517@gmail.com>
2026-09-25 00:49:30 +0300

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import model.Nurse;
-
-public class Main {
-    public static void main(String[] args) {
-        Nurse nurse = new Nurse("N-01", "pass123", "Fatema Begum", "fatema@gmail.com", "01712345678", "Morning", "Cardiology");
-        System.out.println("Nurse Details");
-        nurse.displayNurseInfo();
-    }
-}
