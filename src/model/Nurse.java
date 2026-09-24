44983edcec2a5efc98b502a02d4e547b7d36986e
Lamia Nusrat Mim <mimlamianusrat517@gmail.com>
2026-09-24 23:13:32 +0300

    Update Nurse class changes

--- src/model/Nurse.java
+++ src/model/Nurse.java
@@ -1,30 +1,33 @@
 package model;
-
-public class Nurse {
-    private  String shift;
-    private  String department;
-    public Nurse(){
-
+public class Nurse extends User {
+    private String shift;
+    private String department;
+    public Nurse() {
+        super();
+        setRole(User.NURSE);
     }
-    public Nurse(String shift,String department){
+    public Nurse(String userId, String password, String name, String email, String contactNo, String shift, String department) {
+        super(userId, password, name, email, contactNo, User.NURSE);
         this.shift = shift;
         this.department = department;
     }
-    public String getShift(){
+    public String getShift() {
         return shift;
     }
-    public String getDepartment(){
-        return department;
-    }
-
     public void setShift(String shift) {
         this.shift = shift;
     }
-
+    public String getDepartment() {
+        return department;
+    }
     public void setDepartment(String department) {
         this.department = department;
     }
-    public void displayNurseInfo(){
+    public void displayNurseInfo() {
+        System.out.println("User ID: " + getUserId());
+        System.out.println("Name: " + getName());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Contact No: " + getContactNo());
         System.out.println("Shift: " + shift);
         System.out.println("Department: " + department);
     }
