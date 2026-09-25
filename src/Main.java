a7e6b81a00cdf9083a65b749e07426d54ca6cb47
jannatul-jerin <2023100000682@seu.edu.bd>
2026-09-25 11:11:31 +0600

    Update Main.java
    Call Patient From Main Func

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import model.User;
 import model.Patient;
 
 public class Main {
 
     public static void main(String[] args) {
-        patientApp();
+        patientFunc();
     }
 
-    public static void patientApp() {
-        System.out.println("\n--- Patient Details ---");
-        Patient patient = new Patient("Female", 24, "123 Main Street, Dhaka");
-        patient.displayPatientInfo();
+    private static void patientFunc() {
+        System.out.println("=== PATIENT Info ===");
+
+        Patient patient = new Patient("P201", "patientPass123", "Jerin", "jerin@hospital.com", "01800000000", User.PATIENT, "24", "Female", "B+", "None");
+        
+        System.out.println("User ID    : " + patient.getUserId());
+        System.out.println("Name       : " + patient.getName());
+        System.out.println("Age        : " + patient.getAge());
+        System.out.println("Gender     : " + patient.getGender());
+        System.out.println("Blood Group: " + patient.getBloodGroup());
+        System.out.println("History    : " + patient.getDiseaseHistory());
     }
 }
