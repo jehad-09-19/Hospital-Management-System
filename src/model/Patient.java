591c46a2d31be2abb29f32dc270f8d856e2927ff
jannatul-jerin <2023100000682@seu.edu.bd>
2026-09-25 11:09:09 +0600

    Update Patient.java
    patients model implemented

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,47 +1,96 @@
 package model;
 
-public class Patient {
+public class Patient extends User
+{
+    private String age;
     private String gender;
-    private int age;
-    private String address;
+    private String bloodGroup;
+    private String diseaseHistory;
 
-    public Patient() {
-
+    public Patient()
+    {
+        super();
+        this.role = User.PATIENT;
     }
 
-    public Patient(String gender, int age, String address) {
-        this.gender = gender;
+    public Patient(String userId, String password, String name, String email, String contactNo, int role, String age, String gender, String bloodGroup, String diseaseHistory)
+    {
+        super(userId, password, role);
+        this.name = name;
+        this.email = email;
+        this.contactNo = contactNo;
         this.age = age;
-        this.address = address;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
+        this.bloodGroup = bloodGroup;
+        this.diseaseHistory = diseaseHistory;
     }
 
-    public void setAge(int age) {
+    public void setAge(String age)
+    {
         this.age = age;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setGender(String gender)
+    {
+        this.gender = gender;
     }
 
-    public void displayPatientInfo() {
-        System.out.println("Gender: " + gender);
-        System.out.println("Age: " + age);
-        System.out.println("Address: " + address);
+    public void setBloodGroup(String bloodGroup)
+    {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public void setDiseaseHistory(String diseaseHistory)
+    {
+        this.diseaseHistory = diseaseHistory;
+    }
+
+    public String getAge()
+    {
+        return this.age;
+    }
+
+    public String getGender()
+    {
+        return this.gender;
+    }
+
+    public String getBloodGroup()
+    {
+        return this.bloodGroup;
+    }
+
+    public String getDiseaseHistory()
+    {
+        return this.diseaseHistory;
+    }
+
+    public String toStringPatient()
+    {
+        String str = this.userId + "," + this.name + "," + this.email + "," + this.contactNo + "," + this.age + "," + this.gender + "," + this.bloodGroup + "," + this.diseaseHistory + "\n";
+        return str;
+    }
+
+    public Patient formPatient(String str)
+    {
+        String information[] = str.split(",");
+
+        if (information.length >= 8)
+        {
+            Patient p = new Patient();
+            p.setUserId(information[0]);
+            p.setName(information[1]);
+            p.setEmail(information[2]);
+            p.setContactNo(information[3]);
+            p.setAge(information[4]);
+            p.setGender(information[5]);
+            p.setBloodGroup(information[6]);
+            p.setDiseaseHistory(information[7].trim());
+            return p;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
